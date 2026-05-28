f9f3ca8237f89915f0124aa5eb415c617f90d137
Pj Contas <pjassinaturas@gmail.com>
2026-05-28 19:35:44 +0000

    ai

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,17 +2,13 @@
 'use client';
 
 /**
- * COLE AQUI AS CHAVES DO SEU PROJETO
- * 
- * 1. Vá em Configurações do Projeto (ícone da engrenagem no menu esquerdo).
- * 2. Role até "Seus apps" e clique no ícone </>.
- * 3. Copie os valores do objeto 'firebaseConfig' e cole abaixo.
+ * Configurações oficiais do projeto Receptor Israel
  */
 export const firebaseConfig = {
-  apiKey: "COLE_AQUI_API_KEY",
-  authDomain: "COLE_AQUI_AUTH_DOMAIN",
-  projectId: "COLE_AQUI_PROJECT_ID",
-  storageBucket: "COLE_AQUI_STORAGE_BUCKET",
-  messagingSenderId: "COLE_AQUI_SENDER_ID",
-  appId: "COLE_AQUI_APP_ID"
+  apiKey: "AIzaSyA1guhqMl-rbQO7KjM2yEzZUyf2bOQO4Qw",
+  authDomain: "receptor-israel.firebaseapp.com",
+  projectId: "receptor-israel",
+  storageBucket: "receptor-israel.firebasestorage.app",
+  messagingSenderId: "656831095354",
+  appId: "1:656831095354:web:2d483808cf284d8bc7cb03"
 };
